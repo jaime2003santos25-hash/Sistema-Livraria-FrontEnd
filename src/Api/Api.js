09f8c2fd16ec33c8072c5e13bcfb7b884a147326
Rodrigo-Rodrigues-Silva <rodrigonazireu@gmail.com>
2026-06-11 20:05:05 -0300

    config: configurar variavel de ambiente para a API

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-
-  baseURL: 'http://localhost:8080'
-
+  // Se existir a variável de ambiente do Vercel, usa ela. 
+  // Se não existir (como no seu PC), ele usa o localhost por padrão.
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080'
 })
 
 export default api
